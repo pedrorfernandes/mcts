aa1201fe29eefb921cce688245f2fa8a2876f9c7
Pedro Fernandes <pedro.fernandes@blip.pt>
2015-11-12 20:07:32 +0000

    implemented my own version of mcts

--- mcts/index.js
+++ mcts/index.js
@@ -4,76 +4,161 @@
 
 var _ = require('lodash');
 
-function Node(game, parent, move, depth, mcts) {
-  this.game = game;
-  this.mcts = mcts;
-  this.parent = parent;
-  this.move = move;
+function Node(options) {
+  this.game = _.extend(new options.game.constructor(), _.cloneDeep(options.game));
+  this.mcts = options.mcts;
+  this.parent = options.parent || null;
+  this.move = typeof options.move != 'undefined' ? options.move : null;
   this.wins = 0;
   this.visits = 0;
   this.children = null;
-  this.depth = depth || 0;
+  this.depth = options.depth || 0;
+  this.possibleMoves = null;
+
+  if (this.move !== null) {
+    this.game.performMove(this.move);
+  }
 }
 
-Node.prototype.getUCB1 = function () {
-  // See https://en.wikipedia.org/wiki/Monte_Carlo_tree_search#Exploration_and_exploitation
-  return (this.wins / this.visits) + Math.sqrt(2 * Math.log(this.parent.visits) / this.visits);
+function isNotExpanded(node) {
+  return node === null;
+}
+
+Node.prototype.isExpandable = function() {
+  return !this.isTerminal() && _.some(this.getChildNodes(), isNotExpanded);
 };
 
-Node.prototype.getChildren = function () {
-  if (this.children === null) {
-    if (this.move !== null) {
-      this.game.performMove(this.move);
+Node.prototype.isTerminal = function() {
+  return this.game.getWinner() != null || _.isEmpty(this.getChildNodes());
+};
+
+Node.prototype.expand = function() {
+  var children = this.getChildNodes();
+
+  var possibleMovesIndexArray = this.possibleMoves.map(function(value, index) {
+    return index;
+  });
+
+  var moveIndex = _(possibleMovesIndexArray)
+    .filter(function(index) {
+      return isNotExpanded(children[index]);
+    })
+    .sample();
+
+  var expanded = new Node({
+    game: this.game,
+    parent: this,
+    move: this.possibleMoves[moveIndex],
+    depth: this.depth + 1,
+    mcts: this.mcts,
+    expanded: true
+  });
+
+  children[moveIndex] = expanded;
+
+  return expanded;
+};
+
+function treePolicy(node) {
+  while(!node.isTerminal()) {
+    if (node.isExpandable()) {
+      return node.expand();
     }
-    this.children = _.map(this.game.getPossibleMoves(), function (move) {
-      return new Node(_.assign(new this.game.constructor(), _.cloneDeep(this.game)), this, move, this.depth + 1, this.mcts);
-    }, this);
+    else {
+      node = node.bestChild(EXPLORATION_VALUE);
+    }
+  }
+  return node;
+}
+
+Node.prototype.getReward = function() {
+  if (this.mcts.player === this.game.getWinner()) {
+    return 1;
+  }
+  return 0;
+};
+
+Node.prototype.pickChild = function() {
+  var move = _.sample(this.game.getPossibleMoves());
+
+  return new Node({
+    game: this.game,
+    parent: this,
+    move: move,
+    depth: this.depth + 1,
+    mcts: this.mcts
+  });
+};
+
+function defaultPolicy(node) {
+  while(!node.isTerminal()) {
+    node = node.pickChild();
+  }
+  return node.getReward();
+}
+
+Node.prototype.backPropagate = function(reward) {
+  var node = this;
+  while (node != null) {
+    node.visits += 1;
+    node.wins += reward;
+    node = node.parent;
+  }
+};
+
+Node.prototype.getChildNodes = function() {
+  if (!this.children) {
+    this.possibleMoves = this.game.getPossibleMoves();
+    this.children = _.fill(new Array(this.possibleMoves.length), null);
   }
   return this.children;
 };
 
-Node.prototype.getWinner = function () {
-  // forces the move to be performed
-  this.getChildren();
-  return this.game.getWinner();
+Node.prototype.bestChild = function(explorationValue) {
+  return _(this.getChildNodes())
+    .shuffle()
+    .max(nodeValue.bind(null, explorationValue));
 };
 
-Node.prototype.nextMove = function () {
-  // shuffle because sortBy is a stable sort but we want equal nodes to be chosen randomly
-  return _(this.getChildren()).shuffle().sortBy(this.mcts.nodeSort).last();
+var nodeValue = function(explorationValue, node) {
+  if (node.parent.game.getCurrentPlayer() === node.mcts.player) {
+    return getUCB1(explorationValue, node);
+  }
+  return - node.visits;
 };
 
-function MCTS(game, rounds, player) {
-  var self = this;
+var EXPLORATION_VALUE = 0.70710678118;
+var NO_EXPLORATION = 0;
+var getUCB1 = function (explorationValue, node) {
+  if (explorationValue !== 0) {
+    return (node.wins / node.visits)
+      + explorationValue * Math.sqrt(2 * Math.log(node.parent.visits) / node.visits);
+  }
+  else {
+    return (node.wins / node.visits);
+  }
+};
+
+function MCTS(game, iterations, player) {
   this.game = game;
-  this.nodeSort = function (node) {
-    if (node.parent.game.getCurrentPlayer() === self.player) {
-      return node.getUCB1();
-    }
-    return -node.visits;
-  };
-  this.rounds = rounds || 1000;
+  this.iterations = iterations || 1000;
   this.player = player || 0;
-  this.rootNode = new Node(game, null, null, 0, this);
 }
 
 MCTS.prototype.selectMove = function () {
-  var round, currentNode;
-  for (round = 0; round < this.rounds; round += 1) {
-    currentNode = this.rootNode;
-    this.rootNode.visits += 1;
-    while (!_.isEmpty(currentNode.getChildren())) {
-      currentNode = currentNode.nextMove();
-      currentNode.visits += 1;
-    }
-    if (this.player === currentNode.getWinner()) {
-      while (currentNode.parent) {
-        currentNode.wins += 1;
-        currentNode = currentNode.parent;
-      }
-    }
+  this.rootNode = new Node({
+    game: this.game,
+    depth: 0,
+    mcts: this
+  });
+
+  for(var i = 0; i < this.iterations; i ++) {
+    var selectedNode = treePolicy(this.rootNode);
+    var reward = defaultPolicy(selectedNode);
+    selectedNode.backPropagate(reward);
   }
-  return _(this.rootNode.getChildren()).sortBy('visits').last().move;
+
+  return this.rootNode.bestChild(NO_EXPLORATION).move;
 };
 
 exports.MCTS = MCTS;
--- test/test_mcts.js
+++ test/test_mcts.js
@@ -16,29 +16,38 @@ describe('mcts', function () {
     var mcts = new MCTS(new SingleCellGame());
     assert.deepEqual(mcts.selectMove(), 0);
   });
+
   it('should always select the winning option when there are two options', function () {
     var mcts = new MCTS(new TwoCellGame());
     assert.equal(mcts.selectMove(), 1);
   });
+
   it('should favor the winning move in a game of Tic Tac Toe', function () {
-    var tictactoegame = new TicTacToeGame(),
-      mcts = new MCTS(tictactoegame, 1000, 'X');
-    tictactoegame.board = [['O',   'O', null],
-                           ['X',   'X', null],
-                           ['O',   'O', null]];
+    var tictactoegame = new TicTacToeGame();
+    var mcts = new MCTS(tictactoegame, 1000, 'X');
+    tictactoegame.board = [
+      ['O',   'O', null],
+      ['X',   'X', null],
+      ['O',   'O', null]
+    ];
     assert.deepEqual(mcts.selectMove(), [1, 2]);
   });
+
   it('should block the winning move in a game of Tic Tac Toe', function () {
-    var tictactoegame = new TicTacToeGame(),
-      mcts = new MCTS(tictactoegame, 10000, 'X');
-    tictactoegame.board = [[null, null,  'O'],
-                           [null,  'O', null],
-                           [null, null, null]];
+    var tictactoegame = new TicTacToeGame();
+    var mcts = new MCTS(tictactoegame, 1000, 'X');
+    tictactoegame.board = [
+      [null, null,  'O'],
+      [null,  'O', null],
+      [null, null, null]
+    ];
     assert.deepEqual(mcts.selectMove(), [2, 0]);
+
     // Make sure opponent moves are visited in order
-    _.forEach(mcts.rootNode.getChildren(), function (node) {
-      var childVisits = _(node.getChildren()).pluck('visits'),
-        maxChildVisits = childVisits.max();
+    _.forEach(mcts.rootNode.getChildNodes(), function (node) {
+      var childVisits = _(node.getChildNodes()).pluck('visits');
+      var maxChildVisits = childVisits.max();
+
       assert.ok(childVisits.every(function (visits) {
         return _.inRange(visits, maxChildVisits - 1, maxChildVisits + 1);
       }));
