e3ae16b1c9c3cad6f33935c23a095cb8995a21c0
Pedro Fernandes <pedro.fernandes@blip.pt>
2016-05-14 16:08:24 +0100

    improved chart output: show a chart for each game in the database

--- treeviz/charts/charts-output.js
+++ treeviz/charts/charts-output.js
@@ -14,23 +14,23 @@ function getWorkingDatabase(bucket) {
 
     let bucketQuery = Promise.promisify(bucket.query, { context: bucket });
 
-    function getWinnersOrderedByGameTime() {
+    function getWinnersOrderedByTimestamp() {
         let query = N1qlQuery.fromString(`
-          select default.game.winners from default where type = 'game' order by timestamp asc
+          select default.game.winners, default.game.gameClass from default where type = 'game' order by timestamp asc
         `);
         return bucketQuery(query);
     }
 
-    function getScoresOrderedByGameTime() {
+    function getScoresOrderedByTimestamp() {
         let query = N1qlQuery.fromString(`
-          select default.game.score from default where type = 'game' order by timestamp asc
+          select default.game.score, default.game.gameClass from default where type = 'game' order by timestamp asc
         `);
         return bucketQuery(query);
     }
 
     return {
-        getWinnersOrderedByGameTime: getWinnersOrderedByGameTime,
-        getScoresOrderedByGameTime: getScoresOrderedByGameTime
+        getWinnersOrderedByTimestamp: getWinnersOrderedByTimestamp,
+        getScoresOrderedByTimestamp: getScoresOrderedByTimestamp
     }
 }
 
@@ -54,31 +54,82 @@ let databaseInstancePromise = new Promise(function (resolve, reject) {
 databaseInstancePromise.then(dbInstance => {
 
     let players = ['Player 1', 'Player 2', 'Player 3', 'Player 4'];
-    let winRateOverGames = { 'Player 1': [], 'Player 2': [], 'Player 3': [], 'Player 4': [] };
-    let scoresOverGames = { 'Player 1': [], 'Player 2': [], 'Player 3': [], 'Player 4': [] };
-    let winCount = { 'Player 1': 0, 'Player 2': 0, 'Player 3': 0, 'Player 4': 0 };
-    let totalGames = 0;
 
-    let getWinners = dbInstance.getWinnersOrderedByGameTime().then(winnersList => {
+    let botNames = [
+        'ISMCTS 10k iterations',
+        'Det. UCT 500 iterations * 40 determinizations',
+        'ISMCTS 10k iterations',
+        'Det. UCT 500 iterations * 40 determinizations'
+    ];
+
+    let title = botNames[0] + ' VS ' + botNames[1];
+
+    let cumulativeWinRate = {
+        'Sueca': { 'Player 1': [], 'Player 2': [], 'Player 3': [], 'Player 4': [] },
+        'Bisca': { 'Player 1': [], 'Player 2': [] },
+        'Hearts': { 'Player 1': [], 'Player 2': [], 'Player 3': [], 'Player 4': [] }
+    };
+
+    let sortedScoresDistribution = {
+        'Sueca': { 'Player 1': [], 'Player 2': [], 'Player 3': [], 'Player 4': [] },
+        'Bisca': { 'Player 1': [], 'Player 2': [] },
+        'Hearts': { 'Player 1': [], 'Player 2': [], 'Player 3': [], 'Player 4': [] }
+    };
+
+    let winCount = {
+        'Sueca': { 'Player 1': 0, 'Player 2': 0, 'Player 3': 0, 'Player 4': 0 },
+        'Bisca': { 'Player 1': 0, 'Player 2': 0 },
+        'Hearts': { 'Player 1': 0, 'Player 2': 0, 'Player 3': 0, 'Player 4': 0 }
+    };
+
+    let totalGames = {
+        'Sueca': 0,
+        'Bisca': 0,
+        'Hearts': 0
+    };
+
+    let numberOfPlayersToTie = {
+        'Sueca': 4,
+        'Bisca': 2,
+        'Hearts': 4
+    };
+
+    function isTie(game, winners) {
+        return numberOfPlayersToTie[game] === winners.length;
+    }
+
+    function isWinner(playerIndex, winners) {
+        return winners.indexOf(playerIndex + 1) > -1;
+    }
+
+    let getWinners = dbInstance.getWinnersOrderedByTimestamp().then(winnersList => {
 
         winnersList.forEach(winningPlayersObject => {
-            let winningPlayers = winningPlayersObject.winners;
-            totalGames += 1;
+            let gameWinners = winningPlayersObject.winners;
+            let game = winningPlayersObject.gameClass;
+
+            totalGames[game] += 1;
 
             players.forEach((player, playerIndex) => {
-                if (winningPlayers.indexOf(playerIndex + 1) > -1) {
-                    winCount[player] += 1;
+                if (isWinner(playerIndex, gameWinners) && !isTie(game, gameWinners)) {
+                    winCount[game][player] += 1;
                 }
 
-                winRateOverGames[player].push(( winCount[player] / totalGames ) * 100);
+                let playerCumulativeWinRate = cumulativeWinRate[game][player];
+
+                if (playerCumulativeWinRate) {
+                    let currentWinRate = ( ( winCount[game][player] / totalGames[game] ) * 100 );
+                    playerCumulativeWinRate.push(currentWinRate);
+                }
             });
         });
     });
 
-    let getScores = dbInstance.getScoresOrderedByGameTime().then(scoresList => {
+    let getScores = dbInstance.getScoresOrderedByTimestamp().then(scoresList => {
 
         scoresList.forEach(scoresObject => {
             let scores = scoresObject.score;
+            let game = scoresObject.gameClass;
 
             scores.forEach((score, playerIndex) => {
                 let player = players[playerIndex];
@@ -87,18 +138,42 @@ databaseInstancePromise.then(dbInstance => {
                     // fix for hearts score bug
                     score = score[0];
                 }
-                scoresOverGames[player].push(score);
+                sortedScoresDistribution[game][player].push(score);
             });
         });
 
-        scoresOverGames = _.mapValues(scoresOverGames, scores => scores.sort((a,b) => a - b));
+        sortedScoresDistribution = _.mapValues(sortedScoresDistribution, (playerScores) =>
+          _.mapValues(playerScores, scores => scores.sort((a,b) => a - b)));
     });
 
     Promise.all([getWinners, getScores]).then(function () {
+        function toDataColumns(dataObject) {
+            return Object.keys(dataObject).map(function (playerKey) {
+                return [playerKey].concat(dataObject[playerKey]);
+            });
+        }
+
+        function decorateBotNames(playerScores, playerName) {
+            return playerName + ' (' + botNames[players.indexOf(playerName)] + ')';
+        }
+
+        function decorateBotNamesInGame(scores) {
+            return _.mapValues(scores, playerScores => _.mapKeys(playerScores, decorateBotNames));
+        }
+
+        cumulativeWinRate = decorateBotNamesInGame(cumulativeWinRate);
+        sortedScoresDistribution = decorateBotNamesInGame(sortedScoresDistribution);
+
+        let cumulativeWinRateColumns = _.mapValues(cumulativeWinRate,
+          playerWinRates => toDataColumns(playerWinRates));
+        let sortedScoresDistributionColumns = _.mapValues(sortedScoresDistribution,
+          playerScores => toDataColumns(playerScores));
+
         let template = fs.readFileSync(__dirname + '/victory-charts.mustache.html', 'utf8');
         let output = Mustache.render(template, {
-            winRateOverGames: JSON.stringify(winRateOverGames),
-            scoresOverGames: JSON.stringify(scoresOverGames)
+            winRateOverGames: JSON.stringify(cumulativeWinRateColumns),
+            scoresOverGames: JSON.stringify(sortedScoresDistributionColumns),
+            title: title
         }).replace(/&quot;/g, '\'');
 
         fs.writeFileSync(__dirname + '/victory-charts.html', output);
